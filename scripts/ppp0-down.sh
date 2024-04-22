f47fe4c698ba0c60363dc3130ee6608e0e6ea359
Andrey Sidorov <13andry06@gmail.com>
2024-04-22 16:55:19 +0400

    Added comment

--- scripts/ppp0-down.sh
+++ scripts/ppp0-down.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# Not used, replaced by hotplug script
+# This script executes when ppp0 is down
 uci del dhcp.cfg01411c.server
 uci add_list dhcp.cfg01411c.server='/server/192.168.0.1'
 uci add_list dhcp.cfg01411c.server='/lan/192.168.0.1'
