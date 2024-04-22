bf4d09ecfec4aa65da6de5d2c082a53cebdffd0d
Andrey Sidorov <13andry06@gmail.com>
2024-04-22 16:49:49 +0400

    Initial commit

---
+++ scripts/ppp0-cron.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# Set vars
+piholeDNS='192.168.0.30'
+backupDNS='192.168.100.1'
+testDomain='www.google.com'
+currentDNS="$(uci show dhcp.cfg01411c.server)"
+
+
+
+if [[ "$currentDNS" == *"$piholeDNS"* ]]; then
+  echo "Current DNS is PiHole"
+  if nslookup $testDomain $piholeDNS ; then
+      echo "PiHole test OK"
+  else
+#     echo "PiHole test failed, setting to backupDNS"
+      logger -t cron.info PiHole test failed, setting to backupDNS
+      uci del dhcp.cfg01411c.server
+      uci add_list dhcp.cfg01411c.server='/server/192.168.0.1'
+      uci add_list dhcp.cfg01411c.server='/lan/192.168.0.1'
+      uci add_list dhcp.cfg01411c.server='/local/192.168.0.1'
+      uci add_list dhcp.cfg01411c.server='192.168.100.1'
+      uci commit dhcp
+      service dnsmasq restart
+  fi
+else
+  echo "Current DNS is ISP"
+  if nslookup $testDomain $piholeDNS ; then
+#     echo "PiHole test OK, switching to PiHole"
+      logger -t cron.info PiHole test OK, switching to PiHole
+      uci del dhcp.cfg01411c.server
+      uci add_list dhcp.cfg01411c.server='/server/192.168.0.1'
+      uci add_list dhcp.cfg01411c.server='/lan/192.168.0.1'
+      uci add_list dhcp.cfg01411c.server='/local/192.168.0.1'
+      uci add_list dhcp.cfg01411c.server='192.168.0.30'
+      uci commit dhcp
+      service dnsmasq restart
+  fi
+fi
---
+++ scripts/ppp0-down.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+uci del dhcp.cfg01411c.server
+uci add_list dhcp.cfg01411c.server='/server/192.168.0.1'
+uci add_list dhcp.cfg01411c.server='/lan/192.168.0.1'
+uci add_list dhcp.cfg01411c.server='/local/192.168.0.1'
+uci add_list dhcp.cfg01411c.server='192.168.100.1'
+uci commit dhcp
+service dnsmasq restart
